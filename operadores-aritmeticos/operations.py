1f842dfc096e80d913fdb3362d7884231975dab7
Paulo Ricardo <paulo.tolkien@outlook.com>
2024-02-19 22:01:24 -0300

    operações aritmeticas em python

---
+++ operadores-aritmeticos/operations.py
@@ -0,0 +1,32 @@
+# Soma
+a = 10
+b = 5
+
+print(f"A soma dos números {a} e {b} é:")
+resultado = a + b
+print(f"Resultado: {resultado}")
+
+# Subtração
+print(f"A subtração dos números {a} e {b} é:")
+resultado = a - b
+print(f"Resultado: {resultado}")
+
+# Multiplicação
+print(f"A multiplicação dos números {a} e {b} é:")
+resultado = a * b
+print(f"Resultado: {resultado}")
+
+# Divisão
+print(f"A divisão dos números {a} e {b} é:")
+resultado = a / b
+print(f"Resultado: {resultado}")
+
+# Exponenciação
+print(f"A exponenciação do número {a} elevado a {b} ( ex: x²) é:")
+resultado = a ** b
+print(f"Resultado: {resultado}")
+
+# Resto da divisão
+print(f"A o resto da divisão dos números {a} e {b} é:")
+resultado = a % b
+print(f"Resultado: {resultado}")
