2d83eadb92b6d105002ad52ea04c1f9ff1bcd883
Paulo Ricardo <paulo.tolkien@outlook.com>
2024-02-22 21:25:23 -0300

    Convertendo temperatura Celsius

--- Program/FirstProject.py
+++ Program/FirstProject.py
@@ -5,6 +5,11 @@ print(f"O valor de x é: {x}")
 nome = input("Digite seu nome:")
 print("Bem vindo:", nome)
 
-print(eval('4 > 10'))
+print("Este programa converte temperatura Celsius em Fahrenheit")
+celsius = input("Digite uma temperatura Cº:")
 
+## A formula é:
+## F = 1.8 * C + 32
+fahrenheit = 1.8 * float(celsius) + 32
+print(f"A temperatura Cº: {celsius} convertida para Fahrenheint é: {fahrenheit}")
 
