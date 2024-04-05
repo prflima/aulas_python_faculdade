043bd570394c585aecbb8f63739d8de3a587f976
Paulo Ricardo <paulo.tolkien@outlook.com>
2024-04-05 17:03:15 -0300

    Ordenando listas

---
+++ gerenciando-memoria/OrderingList.py
@@ -0,0 +1,13 @@
+# Criando uma lista de consoles videogamess
+
+videogames = ["Nintendo", "Atari", "Playstation", "Mega-Drive"]
+
+# Ordenando forma crescente
+videogames.sort()
+print(videogames)
+
+# Forma decrescente
+videogames.sort(reverse=True)
+print(videogames)
+
+
